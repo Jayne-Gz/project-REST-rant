72eaf77cea8fae4a7574cb2a942f2c341b68a1fe
craftyaj <craftyaj11@gmail.com>
2022-09-27 18:25:33 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,51 @@
 const router = require('express').Router()
+const places = require('. ./models/places.js')
 
-router.get('/', (req, res) => {
-    res.render('places/index')
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if ({place: places[id] }) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id], id })
+  }
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('POST /places')
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/place')
+  }
 })
 
 module.exports = router
